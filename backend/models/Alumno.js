730295bcfa90f7d628d34ac512933e58bb8a8e2e
Roberto <betotc0029@gmail.com>
2022-02-02 19:21:32 -0600

    Inicio de proyecto

---
+++ backend/models/Alumno.js
@@ -0,0 +1,9 @@
+
+function crear(data){
+    let nombre = data.nombre;
+    let apellido_paterno = data.apellido_paterno;
+    let apellido_materno = data.apellido_materno ? data.apellido_materno : ""
+    let fecha_naciemiento = data.fecha_naciemiento;
+    let anio_ingreso = data.anio_ingreso;
+
+}
